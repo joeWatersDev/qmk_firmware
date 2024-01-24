d6f5e9e40d455a0a0b999f8b05ab903101a56226
Joe Waters <108416038+joeWatersDev@users.noreply.github.com>
2024-01-23 21:15:30 -0500

    Create base layer (Colemak) in lulu firmware

--- keyboards/boardsource/lulu/keymaps/joeWatersDev/keymap.c
+++ keyboards/boardsource/lulu/keymaps/joeWatersDev/keymap.c
@@ -13,30 +13,32 @@ enum layers {
 #define SYMBOL TT(_SYMBOL)
 #define NUMPAD TT(_NUMPAD)
 #define GAME TO(_GAME)
+#define COLMAK TO(_COLEMAK)
+#define TAPPING_TOGGLE 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  =   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * | Del  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   '  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * | TAB  |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |   N  |   E  |   I  |   O  |BackSP|
+ * |------+------+------+------+------+------| Vol+/-|    |<- / ->|------+------+------+------+------+------|
+ * |LCtrl |   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   /  |  ;   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   | LGUI |SYMBOL|LShift| /Space  /       \Enter \  | RAlt |NUMPAD| Menu |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
  [_COLEMAK] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                      KC_LALT,    KC_LGUI, LOWER, KC_SPC,           KC_ENT,   RAISE,   KC_BSPC, KC_RGUI
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      KC_EQL,
+  KC_DEL,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,   KC_MINS,
+  KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,      KC_BSPC,
+  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MUTE,  KC_LEFT,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,   KC_SCLN,
+                      KC_LGUI,   SYMBOL, KC_LSFT, KC_SPC,           KC_ENT,   KC_RALT,   NUMPAD, KC_APP
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
